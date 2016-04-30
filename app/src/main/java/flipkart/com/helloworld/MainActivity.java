c84f07eb3ea11ae58a14595bfe2d66c70a58623c
rajtrivedi88 <rajtrivedi88@gmail.com>
2016-04-30 15:23:34 +0530

    added comment to the file

--- app/src/main/java/flipkart/com/helloworld/MainActivity.java
+++ app/src/main/java/flipkart/com/helloworld/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //aadded layout file
         setContentView(R.layout.activity_main);
     }
 }
